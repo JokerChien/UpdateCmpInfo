942da02b575dfb42f764dabc380ae107a9a3009e
JokerChien <0504aslan@163.com>
2018-11-12 21:59:25 +0800

    五月花2018-11-12

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,36 +9,66 @@ using System.IO;
 //using Jayrock.Json.Conversion;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args){
+            
+            string M_str_sqlcon = "server=localhost;user id=root;password=;database=guessing_league"; //根据自己的设置
+            MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+            myCon.Open();
+            MySqlDataAdapter mysda = new MySqlDataAdapter();
+            mysda.SelectCommand = new MySqlCommand();
+            mysda.SelectCommand.Connection = myCon;
+            mysda.SelectCommand.CommandText = "select * from user_list";
+
+            mysda.SelectCommand.ExecuteNonQuery();
+
+            DataSet ds = new DataSet();
+            //Console.WriteLine(ds.Tables[0].Rows[0].ToString());
+
+            //GridView1.DataSource = ds;
+            //GridView1.DataBind();
+
+            mysda.Fill(ds);
+            myCon.Close();
+
+            foreach (DataRow i in ds.Tables[0].Rows) {
+                //Console.WriteLine(i[1].ToString());
+                foreach (var j in i.ItemArray){
+                    Console.WriteLine(j.ToString());
+                }
+            }
 
 
-
-            //return;
+            Console.ReadKey();
+            return;
             string jsonText = HttpGet("http://op.juhe.cn/onebox/football/team?dtype=&team=%E6%9B%BC%E8%81%94&key=c44c8cab0848d9e5fcc35809e45be3ec");
-
-
+            
             JObject jo = (JObject)JsonConvert.DeserializeObject(jsonText);
-
-
+            
             string reason   = jo["reason"].ToString();
             string key      = jo["result"]["key"].ToString();
             string list = jo["result"]["list"].ToString();
             JArray jArray = (JArray)JsonConvert.DeserializeObject(list);//jsonArrayText必须是带[]数组格式字符串
             
-
-            foreach (var i in jo) {
-                Console.WriteLine(i.ToString());
-            }
             
+            foreach (var i in jo) {
+                //Console.WriteLine(i.ToString());
+            }
+
+            foreach (var i in jArray) {
+                Console.WriteLine(i["c1"]);
+            }
+
+
             // Console.WriteLine(jArray[0]["c1"]);
 
-           
+
             //string list     = jo["reason"]["key"]["list"].ToString();
             //string result = jo["result"]["c1"].ToString();
 
