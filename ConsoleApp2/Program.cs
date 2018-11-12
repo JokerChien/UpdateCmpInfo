55637f98f43fe024c458539b9b633ff101c3a3d5
JokerChien <0504aslan@163.com>
2018-11-12 18:19:40 +0800

    春阳里2018-11-12

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
-
+//using Jayrock.Json;
+//using Jayrock.Json.Conversion;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace ConsoleApp2
@@ -16,9 +19,54 @@ namespace ConsoleApp2
 
 
 
-            return;
-            string ss = HttpGet("http://op.juhe.cn/onebox/football/team?dtype=&team=%E6%9B%BC%E8%81%94&key=c44c8cab0848d9e5fcc35809e45be3ec");
-            Console.WriteLine(ss);
+            //return;
+            string jsonText = HttpGet("http://op.juhe.cn/onebox/football/team?dtype=&team=%E6%9B%BC%E8%81%94&key=c44c8cab0848d9e5fcc35809e45be3ec");
+
+
+            JObject jo = (JObject)JsonConvert.DeserializeObject(jsonText);
+
+
+            string reason   = jo["reason"].ToString();
+            string key      = jo["result"]["key"].ToString();
+            string list = jo["result"]["list"].ToString();
+            JArray jArray = (JArray)JsonConvert.DeserializeObject(list);//jsonArrayText必须是带[]数组格式字符串
+            
+
+            foreach (var i in jo) {
+                Console.WriteLine(i.ToString());
+            }
+            
+            // Console.WriteLine(jArray[0]["c1"]);
+
+           
+            //string list     = jo["reason"]["key"]["list"].ToString();
+            //string result = jo["result"]["c1"].ToString();
+
+            //JArray jArray = (JArray)JsonConvert.DeserializeObject(jsonText);//jsonArrayText必须是带[]数组格式字符串
+
+
+
+
+
+            //User user = (User)Jayrock.Json.Conversion.JsonConvert.Import(typeof(User), ss);
+            //var mJObj = JArray.Parse(ss);
+            //foreach (var i in mJObj) {
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine(jsonText);
+            //Console.WriteLine(reason);
+            //Console.WriteLine(key);
+            //Console.WriteLine(list);
+
+            //Console.WriteLine(key);
+            //Console.WriteLine(list);
+            //Console.WriteLine(result);
+            //Console.WriteLine(jArray['result']);
+            //Console.WriteLine(zone);
+            //Console.WriteLine(result);
+            //Console.WriteLine(user);
+            //Console.WriteLine(user.reason);
+
             Console.ReadKey();
         }
 
@@ -39,5 +87,49 @@ namespace ConsoleApp2
         }
     }
 
+    public class User {
+        public User() {
+            //
+            // TODO: 在此处添加构造函数逻辑
+            //
+        }
 
+        private int id;
+
+        public string reason;
+        public string[] result;
+
+        //public string c4r;
+        //public string result;
+        //User result;
+
+        public int Id {
+            get { return id; }
+            set { id = value; }
+        }
+        private string name;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+        private DateTime time;
+
+        public DateTime Time {
+            get { return time; }
+            set { time = value; }
+        }
+        private double money;
+
+        public double Money {
+            get { return money; }
+            set { money = value; }
+        }
+        private string[] str;
+
+        public string[] Str {
+            get { return str; }
+            set { str = value; }
+        }
+    }
 }
